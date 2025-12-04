080907bb7e88ba646a3945d3184086433bbe66cc
Cyril FRANCOIS <128470639+CyrilFrancois@users.noreply.github.com>
2025-12-04 19:35:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from audioToTranscrypt import audioToTranscript
+from audioToTranscript import audioToTranscript
 from transcriptsToSummary import transcriptsToSummary
 
 if __name__ == "__main__":
     audioToTranscript()
+
     transcriptsToSummary()
