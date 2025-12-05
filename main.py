a44a3580daf01a247d9475559045de7e3609ec78
Cyril FRANCOIS <128470639+CyrilFrancois@users.noreply.github.com>
2025-12-05 09:01:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from audioToTranscript import audioToTranscript
 from transcriptsToSummary import transcriptsToSummary
 
 if __name__ == "__main__":
+    print("**Running audioToTranscript**")
     audioToTranscript()
-
+    print("**Running transcriptsToSummary**")
     transcriptsToSummary()
+    print("**Done!**")
